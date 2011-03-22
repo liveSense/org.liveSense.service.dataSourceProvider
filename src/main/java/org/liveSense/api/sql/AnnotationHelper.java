0da858a479b5fb3d888fdd16ec431ba1e09bb747
Robert Csakany <csakany.robert@gmail.com>
2011-03-22 09:03:38 +0100

    AnnotationHelper splitted, some methods moved to core

--- src/main/java/org/liveSense/api/sql/AnnotationHelper.java
+++ src/main/java/org/liveSense/api/sql/AnnotationHelper.java
@@ -3,13 +3,12 @@ package org.liveSense.api.sql;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import org.liveSense.core.BaseAnnotationHelper;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,60 +16,8 @@ import javax.persistence.Id;
 
 import org.apache.commons.beanutils.BeanUtilsBean;
 
-public class AnnotationHelper {
+public class AnnotationHelper extends BaseAnnotationHelper {
 	
-    public static Annotation[] findClassAnnotation(Class<?> clazz) {
-        return clazz.getAnnotations();
-    }
-
-    public static Annotation[] findMethodAnnotation(Class<?> clazz, String methodName) {
-
-        Annotation[] annotations = null;
-        try {
-            Class<?>[] params = null;
-            Method method = clazz.getDeclaredMethod(methodName, params);
-            if (method != null) {
-                annotations = method.getAnnotations();
-            }
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-        return annotations;
-    }
-
-    public static Annotation[] findFieldAnnotation(Class<?> clazz, String fieldName) {
-        Annotation[] annotations = null;
-        try {
-            Field field = clazz.getDeclaredField(fieldName);
-            if (field != null) {
-                annotations = field.getAnnotations();
-            }
-        } catch (SecurityException e) {
-        } catch (NoSuchFieldException e) {
-        }
-        return annotations;
-    }
-
-    public static List<Field> getAllFields(Class<?> type) {
-    	return getAllFields(null, type);
-    }
-
-    
-    private static List<Field> getAllFields(List<Field> fields, Class<?> type) {
-        if (fields == null) fields = new ArrayList<Field>();
-    	for (Field field: type.getDeclaredFields()) {
-            fields.add(field);
-        }
-
-        if (type.getSuperclass() != null) {
-            fields = getAllFields(fields, type.getSuperclass());
-        }
-
-        return fields;
-    }
-    
     public static Map<String, Object> getObjectAsMap(Object bean) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
     	Map<String, Object> ret = new HashMap<String, Object>();
     	
