3c1c7309806b490791ea700d129e5a55cf3a10b5
Molnár Attila <amolnar@mve.hu>
2011-03-24 16:26:50 +0100

    method getObjectAsMap overloads

--- src/main/java/org/liveSense/api/sql/AnnotationHelper.java
+++ src/main/java/org/liveSense/api/sql/AnnotationHelper.java
@@ -3,6 +3,7 @@ package org.liveSense.api.sql;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -19,17 +20,31 @@ import org.apache.commons.beanutils.BeanUtilsBean;
 public class AnnotationHelper extends BaseAnnotationHelper {
 	
     public static Map<String, Object> getObjectAsMap(Object bean) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    	return getObjectAsMap(bean, (List<String>)null);
+    }
+
+    public static Map<String, Object> getObjectAsMap(Object bean, String[] fieldList) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    	List<String> list =  new ArrayList<String>();
+    	for (String field : fieldList) {
+			list.add(field);
+		}
+    	return getObjectAsMap(bean, list);
+    }    
+	
+    public static Map<String, Object> getObjectAsMap(Object bean, List<String> fieldList) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
     	Map<String, Object> ret = new HashMap<String, Object>();
     	
     	List<Field> fields = getAllFields(bean.getClass());
   	
     	for (Field fld : fields) {
-    		Annotation[] annotations = fld.getAnnotations();
-    		for (int i=0; i<annotations.length; i++) {
-    			if (annotations[i] instanceof Column) {
-    				Column col = (Column)annotations[i];
-    				ret.put(col.name(), BeanUtilsBean.getInstance().getPropertyUtils().getNestedProperty(bean, fld.getName()));
-    			}
+    		if ((fieldList ==  null) || (fieldList.size() == 0) || (fieldList.indexOf(fld.getName()) != -1)) {
+	    		Annotation[] annotations = fld.getAnnotations();
+	    		for (int i=0; i<annotations.length; i++) {
+	    			if (annotations[i] instanceof Column) {
+	    				Column col = (Column)annotations[i];
+	    				ret.put(col.name(), BeanUtilsBean.getInstance().getPropertyUtils().getNestedProperty(bean, fld.getName()));
+	    			}
+	    		}
     		}
     	}
     	return ret;
