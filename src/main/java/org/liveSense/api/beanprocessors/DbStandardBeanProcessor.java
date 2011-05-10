8e3b368dbb151d655ee0ea01d759f3f8abec0102
Molnár Attila <amolnar@mve.hu>
2011-05-10 15:53:25 +0200

    remove import (warning)

--- src/main/java/org/liveSense/api/beanprocessors/DbStandardBeanProcessor.java
+++ src/main/java/org/liveSense/api/beanprocessors/DbStandardBeanProcessor.java
@@ -22,7 +22,6 @@ import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.ResultSet;
