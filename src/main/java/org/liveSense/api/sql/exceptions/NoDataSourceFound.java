ac4d4ce4eee59985e10641f4e62da73db5cd50a7
Molnár Attila <amolnar@mve.hu>
2011-05-10 15:55:01 +0200

    serialVersionUID (warning)

--- src/main/java/org/liveSense/api/sql/exceptions/NoDataSourceFound.java
+++ src/main/java/org/liveSense/api/sql/exceptions/NoDataSourceFound.java
@@ -3,6 +3,8 @@ package org.liveSense.api.sql.exceptions;
 public class NoDataSourceFound extends Exception {
 	
 	
+	private static final long serialVersionUID = 5464666306275408477L;
+
 	public NoDataSourceFound(Throwable e) {
 		super(e);
 	}
