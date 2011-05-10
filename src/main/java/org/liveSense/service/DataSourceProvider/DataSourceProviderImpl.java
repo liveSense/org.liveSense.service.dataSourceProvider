4564c8bb7a9f9e5d2686b1f24d63b0ee1704456e
Molnár Attila <amolnar@mve.hu>
2011-05-10 15:54:48 +0200

    remove import (warning)

--- src/main/java/org/liveSense/service/DataSourceProvider/DataSourceProviderImpl.java
+++ src/main/java/org/liveSense/service/DataSourceProvider/DataSourceProviderImpl.java
@@ -7,7 +7,6 @@ package org.liveSense.service.DataSourceProvider;
  */
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.sql.DataSource;
