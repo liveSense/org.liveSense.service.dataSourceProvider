2572f6d1001c1de16ad965e3a826fced4f1a4bb1
Molnár Attila <amolnar@mve.hu>
2011-05-10 15:52:56 +0200

    new bean for tests

---
+++ src/test/java/org/liveSense/api/beanprocessors/TestBean2.java
@@ -0,0 +1,88 @@
+package org.liveSense.api.beanprocessors;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.junit.Ignore;
+
+@Ignore
+@Entity(name="BeanTest2")
+public class TestBean2 {
+	
+	@Id
+	@Column(name="ID", columnDefinition="INTEGER")
+	private Integer id;
+
+	@Column(name="ID_CUSTOMER", columnDefinition="INTEGER")
+	private Integer idCustomer;
+	
+	@Column(name="PASSWORD_ANNOTATED", columnDefinition="VARCHAR(20)")
+	private String confirmationPassword;
+	
+	@Column(name="FOUR_PART_COLUMN_NAME", columnDefinition="INTEGER")
+	private Boolean fourPartColumnName;
+	
+	@Column(name="BLOB_FIELD", columnDefinition="BLOB")
+	private String blob;
+
+	@Column(name="FLOAT_FIELD", columnDefinition="NUMERIC(15,2)")
+	private Double floatField;
+
+	@Column(name="DATE_FIELD_WITH_ANNOTATION", columnDefinition="DATE")
+	private Date dateFieldWithAnnotation;
+
+	private Date dateFieldWithoutAnnotation;
+	
+	public Date getDateFieldWithoutAnnotation() {
+		return dateFieldWithoutAnnotation;
+	}
+	public void setDateFieldWithoutAnnotation(Date dateFieldWithoutAnnotation) {
+		this.dateFieldWithoutAnnotation = dateFieldWithoutAnnotation;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getIdCustomer() {
+		return idCustomer;
+	}
+	public void setIdCustomer(Integer idCustomer) {
+		this.idCustomer = idCustomer;
+	}
+	public String getConfirmationPassword() {
+		return confirmationPassword;
+	}
+	public void setConfirmationPassword(String confirmationPassword) {
+		this.confirmationPassword = confirmationPassword;
+	}
+	public Boolean getFourPartColumnName() {
+		return fourPartColumnName;
+	}
+	public void setFourPartColumnName(Boolean fourPartColumnName) {
+		this.fourPartColumnName = fourPartColumnName;
+	}
+	public String getBlob() {
+		return blob;
+	}
+	public void setBlob(String blob) {
+		this.blob = blob;
+	}
+	public Double getFloatField() {
+		return floatField;
+	}	
+	public void setFloatField(Double floatField) {
+		this.floatField = floatField;
+	}
+	public Date getDateFieldWithAnnotation() {	
+		return dateFieldWithAnnotation;
+	}
+	public void setDateFieldWithAnnotation(
+		Date dateFieldWithAnnotation) {
+		this.dateFieldWithAnnotation = dateFieldWithAnnotation;
+	}
+}
