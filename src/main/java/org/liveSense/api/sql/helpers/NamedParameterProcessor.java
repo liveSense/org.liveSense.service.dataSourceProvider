a3c63b7a77b0dad949323212c62adafc28ea5140
Molnár Attila <amolnar@mve.hu>
2011-09-06 11:54:18 +0200

    helper methods extracted to new class from SQLExecute bugfixes

---
+++ src/main/java/org/liveSense/api/sql/helpers/NamedParameterProcessor.java
@@ -0,0 +1,102 @@
+package org.liveSense.api.sql.helpers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang.StringUtils;
+import org.liveSense.api.sql.exceptions.SQLException;
+
+
+public class NamedParameterProcessor {
+	
+	
+	//CONSTS
+	private static final String UNKNOWN_PARAMETER = "Unknown parameter";
+	
+	
+	//FIELDS
+	private Map<String, List<Integer>> parameters = new HashMap<String, List<Integer>>();
+	private String sqlStatement;	
+
+	
+	//GETTERS AND SETTERS
+	public Map<String, List<Integer>> getParameters() {	
+		return parameters;
+	}
+	
+	public String getSqlStatement() {	
+		return sqlStatement;
+	}	
+	
+	
+	//CONSTRUCTORS
+	public NamedParameterProcessor(String sql) 
+		throws SQLException {
+			
+		int paramCount = 0;
+		int ps = sql.indexOf(':');
+		while (ps != -1) {
+			//check whether we found it in a string literal
+			if (StringUtils.countMatches(sql.substring(1, ps), "'") % 2 == 0) {
+				paramCount++;
+				
+				//get parameter name
+				StringBuffer sb = new StringBuffer();
+				ps++;
+				while ((ps + 1 <= sql.length()) && (sql.substring(ps, ps + 1).matches("[a-zA-Z0-9_]+")))  {
+					sb.append(sql.substring(ps, ps + 1));
+					ps++;
+				}
+				//store position
+				String paramName = sb.toString();
+				List<Integer> list = parameters.get(paramName);
+				if (list == null) {
+					list = new ArrayList<Integer>();
+					parameters.put(paramName, list);					
+				}				
+				list.add(paramCount);
+				StringBuffer sb2 = new StringBuffer(sql);
+				sb2.delete(ps - paramName.length() - 1, ps);
+				sb2.insert(ps - paramName.length() - 1, "?");
+				sql = sb2.toString();
+				ps = ps - paramName.length();
+			}
+			ps = sql.indexOf(':', ps + 1);
+		}
+		sqlStatement = sql;
+	}
+	
+	
+	//METHODS
+	public List<Object> getSQLParameters(Map<String, Object> params) 
+		throws SQLException {
+		
+		ArrayList<Object> list = new ArrayList<Object>();
+		
+		if (params == null) 
+			return list;
+		
+		for (Entry<String, Object> paramsEntry : params.entrySet()) {
+			List<Integer> positions = null;
+			//case insensitive "get"
+			for (Entry<String, List<Integer>> parametersEntry : parameters.entrySet()) {
+				if (parametersEntry.getKey().toLowerCase().equals(paramsEntry.getKey().toLowerCase())) {
+					positions = parametersEntry.getValue();				
+				}				
+			}
+						
+			if (positions == null)
+				throw new SQLException(UNKNOWN_PARAMETER + " : "+paramsEntry.getKey());
+			
+			for (Integer pos : positions) {
+				while (list.size() < pos) list.add(null);
+				list.set(pos - 1, paramsEntry.getValue());
+			}			
+		}
+		return list;
+	}	
+
+}
---
+++ src/test/java/org/liveSense/api/sql/helper/NamedParameterProcessorTest.java
@@ -0,0 +1,68 @@
+package org.liveSense.api.sql.helper;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.liveSense.api.sql.exceptions.SQLException;
+import org.liveSense.api.sql.helpers.NamedParameterProcessor;
+
+
+public class NamedParameterProcessorTest {
+	
+	private static final String SQL = "SELECT * FROM dual WHERE a = :a1 AND b = :b2 AND a + b = :a1 + :b3 AND c = :long_valid_param_name"; 
+	private static final String SQLPROCESSED = "SELECT * FROM dual WHERE a = ? AND b = ? AND a + b = ? + ? AND c = ?";
+	
+	
+	@Test
+	public void constructorOK() 
+		throws SQLException {
+		
+		//tested method
+		NamedParameterProcessor npp = new NamedParameterProcessor(SQL);
+		
+		//tests
+		assertTrue(npp.getSqlStatement().equals(SQLPROCESSED));
+		
+		assertTrue(npp.getParameters().get("a1").size() == 2);
+		assertTrue(npp.getParameters().get("a1").get(0) == 1);
+		assertTrue(npp.getParameters().get("a1").get(1) == 3);
+		
+		assertTrue(npp.getParameters().get("b2").size() == 1);
+		assertTrue(npp.getParameters().get("b2").get(0) == 2);
+		
+		assertTrue(npp.getParameters().get("b3").size() == 1);
+		assertTrue(npp.getParameters().get("b3").get(0) == 4);
+		
+		assertTrue(npp.getParameters().get("long_valid_param_name").size() == 1);
+		assertTrue(npp.getParameters().get("long_valid_param_name").get(0) == 5);
+	}
+	
+	@Test
+	public void getSQLParametersOK()
+		throws SQLException {
+		
+		//prepare
+		NamedParameterProcessor npp = new NamedParameterProcessor(SQL);
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("a1", new Integer(1));
+		params.put("b2", new Integer(2));
+		params.put("b3", new Integer(3));
+		params.put("long_valid_param_name", "D'oh!");
+		
+		//tested method
+		List<Object> sqlParams = npp.getSQLParameters(params);
+		
+		//tests
+		assertTrue(sqlParams.size() == 5);
+		assertTrue((Integer)sqlParams.get(0) == 1);
+		assertTrue((Integer)sqlParams.get(1) == 2);
+		assertTrue((Integer)sqlParams.get(2) == 1);
+		assertTrue((Integer)sqlParams.get(3) == 3);
+		assertTrue(((String)sqlParams.get(4)).equals("D'oh!"));		
+	}
+
+}
