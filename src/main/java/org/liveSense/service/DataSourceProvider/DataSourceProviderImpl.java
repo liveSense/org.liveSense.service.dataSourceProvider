1d6d1af051549c748e49930ce7e4ee7c65c5b017
Robert Csakany <csakany.robert@gmail.com>
2012-02-13 15:15:03 +0100

    ComboBox added for Isolation level in Felix Web Console

--- src/main/java/org/liveSense/service/DataSourceProvider/DataSourceProviderImpl.java
+++ src/main/java/org/liveSense/service/DataSourceProvider/DataSourceProviderImpl.java
@@ -19,13 +19,13 @@ import org.apache.felix.scr.annotations.ConfigurationPolicy;
 import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.PropertyOption;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.liveSense.api.sql.exceptions.NoDataSourceFound;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 /**
  * OSGi DBCP Datasource provider<br/>
@@ -68,138 +68,108 @@ import org.slf4j.LoggerFactory;
  *</table> 
  */
 
- @Component(label="%dataSourceProvider.name",
-        description="%dataSourceProvider.description",
-        metatype=true,
-        configurationFactory=true,
-        policy=ConfigurationPolicy.REQUIRE,
-        createPid=true)
-        
-@Service(value=DataSourceProvider.class,serviceFactory=true)
-
+@Component(label = "%dataSourceProvider.name", description = "%dataSourceProvider.description", metatype = true, configurationFactory = true, policy = ConfigurationPolicy.REQUIRE, createPid = true)
+@Service(value = DataSourceProvider.class, serviceFactory = true)
 @Properties(value = {
-		@Property(
-				name="dataSourceName", 
-				value="default",
-				description="%dataSourceName"),
-		@Property(name="driverClassName",
-				value="com.mysql.jdbc.Driver",
-				description="%driverClassName"),
-		@Property(
-				name="url", 
-				value="jdbc:mysql://localhost:3306/database?characterSetResults=UTF-8&characterEncoding=UTF-8&useUnicode=yes",
-				description="%dataSource.jdbc.connectString.description"),
-		@Property(
-				name="username",
-				value="user",
-				description="%username"),
-		@Property(
-				name="password",
-				value="password",
-				description="%password"),
-		@Property(
-				name="connectionProperties",
-				value="connectionProperties",
-				description="%connectionProperties"),
-		@Property(
-				name="defaultAutoCommit",
-				boolValue=true,
-				description="%connectionProperties"),
-		@Property(
-				name="defaultReadOnly",
-				boolValue=false,
-				description="%defaultReadOnly"),
-		@Property(
-				name="defaultTransactionIsolation",
-				intValue=java.sql.Connection.TRANSACTION_READ_COMMITTED,
-				description="%defaultTransactionIsolation"),
-		@Property(
-				name="initialSize",
-				intValue=0,
-				description="%initialSize"),
-		@Property(
-				name="maxActive",
-				intValue=8,
-				description="%maxActive"),
-		@Property(
-				name="maxIdle",
-				intValue=8,
-				description="%maxIdle"),
-		@Property(
-				name="maxWait",
-				intValue=-1,
-				description="%maxWait"),
-		@Property(
-				name="validationQuery",
-				value="",
-				description="%validationQuery")
-	}
-)
- 
-
+		@Property(name = "dataSourceName", value = "default", description = "%dataSourceName"),
+		@Property(name = "driverClassName", value = "com.mysql.jdbc.Driver", description = "%driverClassName"),
+		@Property(name = "url", value = "jdbc:mysql://localhost:3306/database?characterSetResults=UTF-8&characterEncoding=UTF-8&useUnicode=yes", description = "%dataSource.jdbc.connectString.description"),
+		@Property(name = "username", value = "user", description = "%username"),
+		@Property(name = "password", value = "password", description = "%password"),
+		@Property(name = "connectionProperties", value = "connectionProperties", description = "%connectionProperties"),
+		@Property(name = "defaultTransactionIsolation",
+			value = DataSourceProviderImpl.ISOLATION_LEVEL_READ_COMMITTED, 
+			options = { 
+				@PropertyOption(name = DataSourceProviderImpl.ISOLATION_LEVEL_NONE, value = "None"),
+				@PropertyOption(name = DataSourceProviderImpl.ISOLATION_LEVEL_READ_COMMITTED, value = "Read-Committed"), 
+				@PropertyOption(name = DataSourceProviderImpl.ISOLATION_LEVEL_READ_UNCOMMITTED, value = "Read-Uncommitted"),
+				@PropertyOption(name = DataSourceProviderImpl.ISOLATION_LEVEL_REPEATABLE_READ, value = "Repeatable-Read"), 
+				@PropertyOption(name = DataSourceProviderImpl.ISOLATION_LEVEL_SERIALIZABLE, value = "Serializable") }, 
+			description = "%defaultTransactionIsolation"),
+		@Property(name = "defaultAutoCommit", boolValue = false, description = "%connectionProperties"), @Property(name = "defaultReadOnly", boolValue = false, description = "%defaultReadOnly"), @Property(name = "initialSize", intValue = 0, description = "%initialSize"),
+		@Property(name = "maxActive", intValue = 8, description = "%maxActive"), @Property(name = "maxIdle", intValue = 8, description = "%maxIdle"), @Property(name = "maxWait", intValue = -1, description = "%maxWait"), @Property(name = "validationQuery", value = "", description = "%validationQuery") })
 public class DataSourceProviderImpl implements DataSourceProvider {
-    private static final Logger log = LoggerFactory.getLogger(DataSourceProviderImpl.class);
+
+	public enum IsolationLevel {
+		NONE(java.sql.Connection.TRANSACTION_NONE), READ_COMMITTED(java.sql.Connection.TRANSACTION_READ_COMMITTED), READ_UNCOMMITTED(java.sql.Connection.TRANSACTION_READ_UNCOMMITTED), REPEATABLE_READ(java.sql.Connection.TRANSACTION_REPEATABLE_READ), SERIALIZABLE(java.sql.Connection.TRANSACTION_SERIALIZABLE);
+		int level;
+
+		private IsolationLevel(int level) {
+			this.level = level;
+		}
+
+		int getLevel() {
+			return level;
+		}
+	}
+
+	public static final String ISOLATION_LEVEL_NONE = "NONE";
+	public static final String ISOLATION_LEVEL_READ_COMMITTED = "READ_COMMITTED";
+	public static final String ISOLATION_LEVEL_READ_UNCOMMITTED = "READ_UNCOMMITTED";
+	public static final String ISOLATION_LEVEL_REPEATABLE_READ = "REPEATABLE_READ";
+	public static final String ISOLATION_LEVEL_SERIALIZABLE = "SERIALIZABLE";
+
+	private static final Logger log = LoggerFactory.getLogger(DataSourceProviderImpl.class);
 	//private static HashMap<String, BasicDataSource> dataSources = new HashMap(); 
-	
 	private BasicDataSource ds;
 	private String dataSourceName = null;
-	
-    @Activate
-    protected void activate(BundleContext bundleContext, Map<?, ?> props) { 
-		String driverClassName = (String)props.get("driverClassName");
-		String url = (String)props.get("url");
-		String username = (String)props.get("username");
-		String password = (String)props.get("password");
-		String connectionProperties = (String)props.get("connectionProperties");
-		Boolean defaultAutoCommit = (Boolean)props.get("defaultAutoCommit");
-		Boolean defaultReadOnly = (Boolean)props.get("defaultReadOnly");
-		Integer defaultTransactionIsolation = (Integer)props.get("defaultTransactionIsolation");
-		Integer initialSize = (Integer)props.get("initialSize");
-		Integer maxActive = (Integer)props.get("maxActive");
-		Integer maxIdle = (Integer)props.get("maxIdle");
-		Integer maxWait = (Integer)props.get("maxWait");
-		String validationQuery = (String)props.get("validationQuery");
-		dataSourceName = (String)props.get("dataSourceName");
-    	if (dataSourceName != null && !"".equalsIgnoreCase(dataSourceName)) {
-        	ds = new BasicDataSource();
-        	log.info("Registering DataSource Name: "+dataSourceName+" PID: "+(String)props.get("service.pid"));
-        	ds.setDriverClassName(driverClassName);
-        	ds.setUrl(url);
-        	ds.setUsername(username);
-        	ds.setPassword(password);
-        	ds.setConnectionProperties(connectionProperties);
-        	ds.setDefaultAutoCommit(defaultAutoCommit);
-        	ds.setDefaultReadOnly(defaultReadOnly);
-        	ds.setDefaultTransactionIsolation(defaultTransactionIsolation);
-        	ds.setInitialSize(initialSize);
-        	ds.setMaxActive(maxActive);
-        	ds.setMaxIdle(maxIdle);
-        	ds.setMaxWait(maxWait);
-        	ds.setValidationQuery(validationQuery);
-    	} else {
-    		log.warn("No data source name is defined PID: "+ (String)props.get("service.pid"));
-    	}
-    }
-    
-    @Deactivate
-    protected void deactivate() {
-    	try {
-    		if (ds != null) {
-    			ds.close();
-    		}
-		} catch (SQLException e) {
-			log.error("Deactivate data source: "+dataSourceName, e);
+
+	@Activate
+	protected void activate(BundleContext bundleContext, Map<?, ?> props) {
+		String driverClassName = (String) props.get("driverClassName");
+		String url = (String) props.get("url");
+		String username = (String) props.get("username");
+		String password = (String) props.get("password");
+		String connectionProperties = (String) props.get("connectionProperties");
+		Boolean defaultAutoCommit = (Boolean) props.get("defaultAutoCommit");
+		Boolean defaultReadOnly = (Boolean) props.get("defaultReadOnly");
+		Integer defaultTransactionIsolation = IsolationLevel.valueOf((String) props.get("defaultTransactionIsolation")).getLevel();
+		Integer initialSize = (Integer) props.get("initialSize");
+		Integer maxActive = (Integer) props.get("maxActive");
+		Integer maxIdle = (Integer) props.get("maxIdle");
+		Integer maxWait = (Integer) props.get("maxWait");
+		String validationQuery = (String) props.get("validationQuery");
+		dataSourceName = (String) props.get("dataSourceName");
+		if (dataSourceName != null && !"".equalsIgnoreCase(dataSourceName)) {
+			ds = new BasicDataSource();
+			log.info("Registering DataSource Name: " + dataSourceName + " PID: " + (String) props.get("service.pid"));
+			ds.setDriverClassName(driverClassName);
+			ds.setUrl(url);
+			ds.setUsername(username);
+			ds.setPassword(password);
+			ds.setConnectionProperties(connectionProperties);
+			ds.setDefaultAutoCommit(defaultAutoCommit);
+			ds.setDefaultReadOnly(defaultReadOnly);
+			ds.setDefaultTransactionIsolation(defaultTransactionIsolation);
+			ds.setInitialSize(initialSize);
+			ds.setMaxActive(maxActive);
+			ds.setMaxIdle(maxIdle);
+			ds.setMaxWait(maxWait);
+			ds.setValidationQuery(validationQuery);
+		} else {
+			log.warn("No data source name is defined PID: " + (String) props.get("service.pid"));
 		}
-    }
-    
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		try {
+			if (ds != null) {
+				ds.close();
+			}
+		} catch (SQLException e) {
+			log.error("Deactivate data source: " + dataSourceName, e);
+		}
+	}
+
 	public Connection getConnection() throws SQLException {
 		return ds.getConnection();
 	}
-	
+
 	public Connection getConnection(String userName, String password) throws SQLException {
-		return ds.getConnection(userName, password);		
+		return ds.getConnection(userName, password);
 	}
-	
+
 	public QueryRunner getQueryRunner() {
 		return new QueryRunner(ds, false);
 	}
@@ -207,8 +177,8 @@ public class DataSourceProviderImpl implements DataSourceProvider {
 	public DataSource getDataSource() {
 		return ds;
 	}
-	
+
 	public String getName() {
 		return dataSourceName;
 	}
-}			
+}
